10afdc2a428b278afec8a145c286073f600bd777
Ofir Shukrun <ofirshukrun13@gmail.com>
2018-06-17 04:37:43 +0300

    Mobile

--- public/assets/js/links.js
+++ public/assets/js/links.js
@@ -1,12 +1,15 @@
-var elisPubMobilePageURL = 'fb://profile/937307902955368';
-var PhizzogMobilePageURL = 'intent://#Intent;package=com.facebook.katana;scheme=fb://page/104448929614076?referrer=app_link;end';
-
+var elisPubMobilePageURLAndroid = 'intent://#Intent;package=com.facebook.katana;scheme=fb://page/937307902955368?referrer=app_link;end';
+var phizzogMobilePageURLAndroid = 'intent://#Intent;package=com.facebook.katana;scheme=fb://page/104448929614076?referrer=app_link;end';
+var elisPubMobilePageURLIphone = 'fb://profile/937307902955368';
+var phizzogMobilePageURLIphone = 'fb://profile/104448929614076';
 
 function onMobile() {
-    console.log('!')
-    if (typeof window.orientation !== 'undefined') {
-        document.getElementById("phizzog-link").href = PhizzogMobilePageURL;
-        document.getElementById("elisPub-link").href = elisPubMobilePageURL;
+    if (/Android|webOS|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        document.getElementById("phizzog-link").href = phizzogMobilePageURLAndroid;
+        document.getElementById("elisPub-link").href = elisPubMobilePageURLAndroid;
+    } else if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+        document.getElementById("phizzog-link").href = phizzogMobilePageURLIphone;
+        document.getElementById("elisPub-link").href = elisPubMobilePageURLIphone;
     }
 };
 
